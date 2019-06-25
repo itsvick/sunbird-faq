0663ff425542d322aabfca59adb36cc3dfd65fe2
Vivek Kasture <vivek_kasture@techjoomla.com>
2019-06-25 16:26:55 +0530

    Removed window.parent

--- dist/resources/js/render-from-json.js
+++ dist/resources/js/render-from-json.js
@@ -1,14 +1,16 @@
 var selectedLang = getUrlVars()["selectedlang"];
+var appName = 'AppName';
+
+if (getUrlVars()["appname"]) {
+    appName = getUrlVars()["appname"];
+}
+
 console.log('selected Lang', selectedLang);
 $(document).ready(function () {
-    var appName = '';
-    if (window.parent.cordova) {
-        window.parent.cordova.getAppVersion.getAppName(function (name) {
-            appName = name;
-        });
-    } else {
-        appName = getUrlVars()["appname"];
-    }
+
+    window.addEventListener('message', function (event) {
+        appName = event.data.name;
+    }, false);
 
     var jsonUrl;
     if (selectedLang) {
@@ -19,6 +21,15 @@ $(document).ready(function () {
     }
 
     console.log(jsonUrl);
+
+    if (appName === 'AppName') {
+        setTimeout(function () {
+            readJson(jsonUrl);
+        }, 100)
+    }
+});
+
+function readJson(jsonUrl) {
     $.getJSON(jsonUrl, function (data) {
         for (var i = 0; i < data.faqs.length; i++) {
             if (data.faqs[i].topic.includes('{{APP_NAME}}')) {
@@ -84,9 +95,8 @@ $(document).ready(function () {
             + '<button class="report-button"><img src="./resources/images/Report.png"></span> ' + data.constants.reportIssueMsg + '</button>'
             + '</div>'
         $('#send-email').replaceWith(html);
-    })
-});
-
+    });
+}
 function getUrlVars() {
     var vars = [], hash;
     var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
